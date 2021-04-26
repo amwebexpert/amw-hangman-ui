eab6d3f31ef8521fcf62e9f44110ca136749f0dd
Andre Masson <amwebexpert@gmail.com>
2021-04-26 13:41:09 -0400

    Added react navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  useColorScheme
+  StyleSheet
 } from 'react-native';
 import { Button, Title } from 'react-native-paper';
-import {
-  Colors
-} from 'react-native/Libraries/NewAppScreen';
 
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Title>My Title</Title>
-      <Button mode="contained" icon="delete">test</Button>
+      <Button mode="contained" icon="delete">Delete button</Button>
     </SafeAreaView>
   );
 };
