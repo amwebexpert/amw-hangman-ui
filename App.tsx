3758c054a2607a8db4a54ae8bb759b3740e1d240
Andre Masson <amwebexpert@gmail.com>
2021-04-26 14:51:07 -0400

    Added react-native-webview

--- App.tsx
+++ App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet
+  StyleSheet,
+  View
 } from 'react-native';
 import { Button, Title } from 'react-native-paper';
 import Config from "react-native-config";
+import WebView from 'react-native-webview';
 
 const App = () => {
+  const serverBaseUrl = Config.HANGMAN_TARGET_SERVER;
+
   return (
     <SafeAreaView style={styles.container}>
       <Title>{Config.HANGMAN_TARGET_SERVER}</Title>
       <Button mode="contained" icon="delete">Delete button example</Button>
+      <View style={styles.webView}>
+        <WebView
+          source={{ uri: serverBaseUrl }}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          sharedCookiesEnabled={true}
+          scalesPageToFit={true}
+          startInLoadingState={true}
+          cacheEnabled={false}
+          cacheMode='LOAD_NO_CACHE'
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -18,8 +34,12 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  webView: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 8,
+    borderColor: 'green',
   },
 });
 
