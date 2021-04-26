87dfbe23ca919be2e3a151cecfc8ece504616aca
Andre Masson <amwebexpert@gmail.com>
2021-04-26 13:54:53 -0400

    Added config, axios, query

--- App.tsx
+++ App.tsx
@@ -4,13 +4,13 @@ import {
   StyleSheet
 } from 'react-native';
 import { Button, Title } from 'react-native-paper';
-
+import Config from "react-native-config";
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Title>My Title</Title>
-      <Button mode="contained" icon="delete">Delete button</Button>
+      <Title>{Config.HANGMAN_TARGET_SERVER}</Title>
+      <Button mode="contained" icon="delete">Delete button example</Button>
     </SafeAreaView>
   );
 };
